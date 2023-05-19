d55687630009e602c2f625b58b18179bb7d1f617
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-05-19 18:13:28 +0000

    Add filters and comment management to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title', 'author',)}
+    list_filter = ('created_on', 'author')
     summernote_fields = ('body')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body', 'post', 'created_on', 'approved')
+    search_fields = ('author', 'body')
+    list_filter = ('created_on', 'approved')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
