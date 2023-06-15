af38821de6f518e02af0929c5e7b9f0409f2fc83
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-06-15 18:25:50 +0000

    Add pagination buttons to index page

--- posts/views.py
+++ posts/views.py
@@ -10,7 +10,7 @@ class PostList(generic.ListView):
     model = Post
     querySet = Post.objects.order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 10
+    paginate_by = 8
 
 
 class PostDetail(View):
