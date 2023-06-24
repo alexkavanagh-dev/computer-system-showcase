9605ebb72ca2408e734dedbe9969f75ccfb617a4
alexkavanagh8 <alexkavanagh8@gmail.com>
2023-06-24 12:55:15 +0000

    Stop author prepopulating slug in admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'created_on')
     search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title', 'author',)}
+    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_on', 'author')
     summernote_fields = ('body')
 
